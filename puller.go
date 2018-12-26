8ceca55fc121ba23e1d67f6d9e26491e974a66db
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-12-26 12:53:39 +0300

    This is my code. My code is amazing.

--- puller.go
+++ puller.go
@@ -3,18 +3,20 @@ package main
 import (
 	"flag"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
 	"os"
+	"strings"
 	"sync"
 )
 
 func main() {
 	fs := flag.NewFlagSet("dockerMegaUtility", flag.ExitOnError)
+	nrun := fs.Bool("run", true, "run all containers")
+	nstop := fs.Bool("stop", true, "stop all containers")
+	ndelete := fs.Bool("delete", true, "delete all containers")
 
-	nrun := fs.Bool("run", false, "run all containers")
-	nstop := fs.Bool("stop", false, "stop all containers")
-	ndelete := fs.Bool("delete", false, "delete all containers")
 
 	err := fs.Parse(os.Args[1:])
 	if err != nil {
@@ -28,13 +30,26 @@ func main() {
 		panic(err)
 	}
 
+	// Args stores filter arguments as map key:{map key: bool}.
+	// It contains an aggregation of the map of arguments (which are in the form
+	// of -f 'key=value') based on the key, and stores values for the same key
+	// in a map with string keys and boolean values.
+	// e.g given -f 'label=label1=1' -f 'label=label2=2' -f 'image.name=ubuntu'
+	// the args will be {"image.name":{"ubuntu":true},"label":{"label1=1":true,"label2=2":true}}
+	args := filters.NewArgs()
+
+	for i := 0; i < len(fs.Args()); i ++ {
+		splitted := strings.Split(fs.Args()[i], "=")
+		args.Add("name", splitted[1])
+	}
+
+
 	if *nstop {
-		stop(ctx, cli)
-		//return
+		stop(ctx, cli, args)
 	}
 
 	if *ndelete {
-		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+		containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All:true, Filters:args})
 		if err != nil {
 			panic(err)
 		}
@@ -42,8 +57,6 @@ func main() {
 		for _, cont := range containers {
 			remove(ctx, cli, cont.ID)
 		}
-
-		//return
 	}
 
 	wg := &sync.WaitGroup{}
@@ -56,7 +69,5 @@ func main() {
 		go redis(ctx, cli, wg)
 		go elastic(ctx, cli, wg)
 	}
-
 	wg.Wait()
-
 }
--- stop.go
+++ stop.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
-func stop(ctx context.Context, cli *client.Client) {
-	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+func stop(ctx context.Context, cli *client.Client, f filters.Args) {
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true, Filters: f})
 	if err != nil {
 		panic(err)
 	}
